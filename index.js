2d3427f2b2e0454e96690c586d415137c9817655
DiwanshGupta <dewanshgupta120@gmail.com>
2024-02-17 22:32:15 +0530

    fix production bugs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://foodyhut.vercel.app"],
     methods: "GET,POST,DELETE,PUT",
     credentials: true,
   })
